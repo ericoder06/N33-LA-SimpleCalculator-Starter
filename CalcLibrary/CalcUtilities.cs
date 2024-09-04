08e85dec0bc1da334c240a7a626719d098ac2d53
ericoder06 <eric.kossovsky@gmail.com>
2024-09-04 10:40:54 -0400

    Added DLL library (CalcUtilities) + Added some methods (add, substract), and referenced the DLL library to the Simple Calculator Project

---
+++ CalcLibrary/CalcUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary
+{
+    public class CalcUtilities
+    {
+        public static decimal add( decimal a, decimal b )
+            { return a + b; }
+
+        public static decimal subtract( decimal a, decimal b ) 
+        { return a - b; }
+
+        
+    }
+}
