2d910d2d8562c3f7696de1ecec04fb0dc437bc7b
ericoder06 <eric.kossovsky@gmail.com>
2024-09-04 11:33:28 -0400

    Reorganised Text on SimpleCalculator Program

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -15,14 +15,14 @@ namespace SimpleCalculator
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
                 Console.WriteLine("Welcome to MyCalculator! \n");
                 Console.WriteLine("Please enter your equation in this order: \n " +
-                    "1.First Number \n 2.Second Number \n 3. Operation");
-                Console.WriteLine("\n Press Enter to start.");
+                    "1. First Number \n 2. Second Number \n 3. Operation");
+                Console.WriteLine("\nPress Enter to start.");
                 Console.ReadLine();
-                Console.WriteLine("Please enter your first Number: ");
+                Console.Write("Please enter your first Number: ");
                 double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                Console.WriteLine("Please enter your second Number: ");
+                Console.Write("Please enter your second Number: ");
                 double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                Console.WriteLine("Please enter the Operation needed");
+                Console.Write("Please enter the Operation needed: ");
                 string operation = Console.ReadLine();
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
