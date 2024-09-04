aed591c04af62ce4e2e99032756658bc4d84bc0c
JuDaidan <Julianduribe0612@gmail.com>
2024-09-04 11:07:47 -0400

    Added Calculate method using Switch to the Calculator Engine.

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -4,14 +4,37 @@ namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
-                result = argFirstNumber + argSecondNumber;
-            }
 
+            switch (argOperation.ToLower())
+            {
+                case "+":
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+
+                    break;
+                case "-":
+                case "substract":
+                    
+                        result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "*":
+                case "multiply":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "/":
+                case "divide":
+                result = argFirstNumber / argSecondNumber;
+                    break;
+
+
+
+            }
             return result;
         }
     }
