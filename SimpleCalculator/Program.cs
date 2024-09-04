002e16519f8314e6d697f705a6c486029177bba4
JuDaidan <Julianduribe0612@gmail.com>
2024-09-04 11:28:12 -0400

    Made the code more human

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -13,9 +13,16 @@ namespace SimpleCalculator
 
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
-
+                Console.WriteLine("Welcome to MyCalculator! \n");
+                Console.WriteLine("Please enter your equation in this order: \n " +
+                    "1.First Number \n 2.Second Number \n 3. Operation");
+                Console.WriteLine("\n Press Enter to start.");
+                Console.ReadLine();
+                Console.WriteLine("Please enter your first Number: ");
                 double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please enter your second Number: ");
                 double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please enter the Operation needed");
                 string operation = Console.ReadLine();
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
